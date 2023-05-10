5efc4cb0ed6b4ea814bd50fcc385676a5794008d
vasilvas99 <vasil.ivanov3@bosch.io>
2023-05-10 09:43:58 +0300

    change license headers to be the same as KAD to help ORT scanning

--- src/rust/kanto-tui/src/ui/mod.rs
+++ src/rust/kanto-tui/src/ui/mod.rs
@@ -1,5 +1,5 @@
-// /********************************************************************************
-// * Copyright (c) 2023 Contributors to the Eclipse Foundation
+// ********************************************************************************
+// * Copyright (c) 2022 Contributors to the Eclipse Foundation
 // *
 // * See the NOTICE file(s) distributed with this work for additional
 // * information regarding copyright ownership.
@@ -9,7 +9,7 @@
 // * https://www.apache.org/licenses/LICENSE-2.0
 // *
 // * SPDX-License-Identifier: Apache-2.0
-// ********************************************************************************/
+// ********************************************************************************
 use cursive::{traits::*, Cursive};
 use super::{
     kantui_config::AppConfig, try_best, KantoRequest, KantoResponse, RequestPriority, Result,
